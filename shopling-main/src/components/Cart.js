4da8c803eef35fb36dfcd647748c6cb3e5181ebe
shuvam <shuvam86>
2022-11-24 11:06:12 +0530

    first

---
+++ shopling-main/src/components/Cart.js
@@ -0,0 +1,161 @@
+import { useEffect, useState } from "react";
+import { Button, Col, Form, Image, ListGroup, Row } from "react-bootstrap";
+import { AiFillDelete } from "react-icons/ai";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+import axios from "axios"
+const Cart = () => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+  const [total, setTotal] = useState();
+  const [name,setName] = useState("")
+  const [email, setEmail] = useState("")
+  
+  const [contact, setContact] = useState(0)
+  function loadScript(src) {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  }
+  async function displayRazorpay() {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
+    const data = {
+         amount : total
+    }
+    const result = await axios.post("http://localhost:5000/payment/orders",data);
+
+    if (!result) {
+      alert("Server error. Are you online?");
+      return;
+    }
+
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+      key: "rzp_test_OI5rxzLeySvb26", // Enter the Key ID generated from the Dashboard
+      amount: amount.toString(),
+      currency: currency,
+      name: "Unknown",
+      description: "Payment to myself",
+      image: "",
+      order_id: order_id,
+      handler: async function (response) {
+        const data = {
+          orderCreationId: order_id,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
+        };
+
+        const result = await axios.post("http://localhost:5000/payment/success", data);
+        console.log(result)
+        document.getElementById("home").innerHTML =`<div><h1>Your transaction has been completed.Status:${result.data.msg}
+        <h1>Your order id is :${result.data.orderId}</h1>
+        <h1>Your payment id is:${result.data.paymentId}</h1>
+        </div>`
+        
+      },
+      prefill: {
+        name: `${name}`,
+        email: `${email}`,
+        contact: `${contact}`,
+      },
+   
+      theme: {
+        color: "#61dafb",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
+  useEffect(() => {
+    setTotal(
+      cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0)
+    );
+  }, [cart]);
+
+  return (
+    <div id="home" className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {cart.map((prod) => (
+            <ListGroup.Item key={prod.id}>
+              <Row>
+                <Col md={2}>
+                  <Image src={prod.image} alt={prod.name} fluid rounded />
+                </Col>
+                <Col md={2}>
+                  <span>{prod.name}</span>
+                </Col>
+                <Col md={2}>₹ {prod.price}</Col>
+                <Col md={2}>
+                  <Rating rating={prod.ratings} />
+                </Col>
+                <Col md={2}>
+                  <Form.Control
+                    as="select"
+                    value={prod.qty}
+                    onChange={(e) =>
+                      dispatch({
+                        type: "CHANGE_CART_QTY",
+                        payload: {
+                          id: prod.id,
+                          qty: e.target.value,
+                        },
+                      })
+                    }
+                  >
+                    {[...Array(prod.inStock).keys()].map((x) => (
+                      <option key={x + 1}>{x + 1}</option>
+                    ))}
+                  </Form.Control>
+                </Col>
+                <Col md={2}>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() =>
+                      dispatch({
+                        type: "REMOVE_FROM_CART",
+                        payload: prod,
+                      })
+                    }
+                  >
+                    <AiFillDelete fontSize="20px" />
+                  </Button>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+      <div className="filters summary">
+        <span className="title">Subtotal ({cart.length}) items</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>Total: ₹ {total}</span>
+        <Button type="button" disabled={cart.length === 0} onClick={displayRazorpay}>
+          Proceed to Checkout
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
